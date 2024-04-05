ac32bee4857cdcd0774e6bed881cd3c88aaa15c0
Kevin Perez Gali <kevin.perez27@upr.edu>
2024-04-04 20:32:03 -0400

    fixed bug with user

--- cafe-rewards-backend/app.js
+++ cafe-rewards-backend/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors')
+const User = require('../models/user');
 
 const app = express();
 
